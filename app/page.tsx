38cffde6250e27f9d5fffdfc913eaaa1a7537dc0
Nik Sumeiko <nik.sumeiko@gmail.com>
2026-05-08 19:02:19 +0200

    feat: Display a list of target

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-'use client'
+'use client';
 
 import { useEffect, useState } from 'react';
 import type { Target } from './voidSession/domain/target';
 import { fetchTargets } from './voidSession/targetsClient/targets.adapter';
+import { toTargetsViewModel } from './voidSession/targetsViewModel/targets.viewModel';
 
 export default function Home() {
   const [targets, setTargets] = useState<Target[]>([]);
@@ -11,18 +12,22 @@ export default function Home() {
     fetchTargets().then(setTargets);
   }, []);
 
+  const viewModels = toTargetsViewModel(targets);
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>Void Session</h1>
         <ul>
-            <li key={`st-002`}>
-              <h2>{`Will Byers`}</h2>
-              <p>Signal clarity: {`57%`}</p>
-              <p>Duration: {`720s`}</p>
-              <p>Last known location: {`Castle Byers`}</p>
+          {viewModels.map((vm) => (
+            <li key={vm.id}>
+              <h2>{vm.name}</h2>
+              <p>Signal clarity: {vm.signalClarity}</p>
+              <p>Duration: {vm.duration}</p>
+              <p>{vm.dimension}</p>
               <button type="button">Focus</button>
             </li>
+          ))}
         </ul>
       </main>
     </div>
